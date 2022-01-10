71ccfb9bb1e0ebf884fa8515ab292ce1523b026e
manikmmalhotra <manikmalhotra2113@gmail.com>
2022-01-10 23:29:56 +0530

    added global theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      lightpuple: "#7b85a6",
+      purple: "#404d73",
+      grey: "#b8c3d9",
+      yellow: "#f2d272",
+      yellowhite: "#f2dfbb",
+      white: "#fff",
+      black: "#000",
+      "gry-dark": "#273444",
+      "graay-light": "#d3dce6",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
